7e27d5334cf67237ece18747154fcded7955e012
Tanveer Ansari <tanveeransari5663@gmail.com>
2026-04-10 10:32:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://remote-interview-backend.vercel.app",
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://remote-interview-backend.vercel.app",
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
 });
